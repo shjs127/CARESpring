b2cadb14b9f0880a62c2fed3aaaebbaa7b107db8
shjs127 <shjs127@hanmail.net>
2021-07-12 15:01:12 +0900

    페이지 이동

--- CARESpring/src/main/java/org/care/controller/UserController.java
+++ CARESpring/src/main/java/org/care/controller/UserController.java
@@ -17,35 +17,33 @@ public class UserController {
 	/*
 	 * @Inject private UserService service;
 	 */
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	  public String mainPage(HttpServletRequest request, Model model){    
+	     
+		  	return "main/index";
+	      }
 
   @RequestMapping(value = "/login", method = RequestMethod.GET)
   public String loginPage(HttpServletRequest request, Model model){
-      // 세션정보에 유저 정보가 있으면 메인페이지로 강제이동
-     
+      
 	  	return "login/login";
-          //return new ModelAndView("login");
       }
   
   @RequestMapping(value = "/board", method = RequestMethod.GET)
   public String boardPage(HttpServletRequest request, Model model){
-      // 세션정보에 유저 정보가 있으면 메인페이지로 강제이동
-     
-	  	return "guestbook/listArticle";
-          //return new ModelAndView("login");
+ 
+	  	return "board/listArticle";
       }
+  
   @RequestMapping(value = "/storeDetail", method = RequestMethod.GET)
-  public String detailPage(HttpServletRequest request, Model model){
-      // 세션정보에 유저 정보가 있으면 메인페이지로 강제이동
+  public String detailPage(HttpServletRequest request, Model model){    
      
 	  	return "main/food-details";
-          //return new ModelAndView("login");
       }
   @RequestMapping(value = "/storeList", method = RequestMethod.GET)
-  public String listPage(HttpServletRequest request, Model model){
-      // 세션정보에 유저 정보가 있으면 메인페이지로 강제이동
+  public String listPage(HttpServletRequest request, Model model){    
      
 	  	return "board/cafeGrid";
-          //return new ModelAndView("login");
       }
 
   // @RequestMapping(value = "/loginPost", method = RequestMethod.POST)
